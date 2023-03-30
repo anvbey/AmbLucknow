a3af946a87003a2af0a495d814ff9b860c29a5de
anvit <anvitgogoa@gmail.com>
2023-03-31 02:13:52 +0530

    Footer updated

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './container/Home';
 import About from './container/About';
+import Footer from './components/Footer/Footer';
 
 function App() {
+  const styles = {
+    backgroundColor: '#f0ebf0',
+  };
   return (
-    <>
+    <div style={styles}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
-    </>
+      <Footer/>
+    </div>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import { Typography } from '@mui/material';
+import { Box } from "@mui/system";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Box sx={{
+        width: 'auto',
+        height: 'fit-content',
+        justifyContent: "center",
+        alignContent: "center",
+        margin: '50px 10px 30px 10px',
+    }}>
+      <Typography
+        variant="subtitle"
+        sx={{
+          color: "#2CA6DB",
+          padding: '50px'
+        }}
+      >
+        Copyright © 2022 Avatar Meher Baba Perpetual Public Charitable Trust,
+        Ahmednagar, India. All quotes of Meher Baba copyright © Avatar Meher
+        Baba Perpetual Public Charitable Trust, unless otherwise indicated.
+        Photos copyright © Meher Nazar Publications and Mani S. Irani, M.S.I.
+        collection; all rights reserved.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
