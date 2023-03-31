797366be082a3c4e71d154b25d753cc2fb23cfb4
anvit <anvitgogoa@gmail.com>
2023-03-31 11:19:40 +0530

    Contact button corrected

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -1,12 +1,23 @@
-import { Button, TextField } from '@mui/material'
+import { Button, InputAdornment, TextField } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Contact = () => {
   return (
     <Box>
-      <TextField label="Your Email" variant="outlined"/>
-      <Button variant='contained'>Contact Us!</Button>
+      <TextField label="Your Email" variant="outlined"  InputProps={{
+        startAdornment: <InputAdornment position='start' ><MailOutlineIcon/></InputAdornment>
+      }}
+      sx={{
+        borderColor : '#1976D2'
+      }}
+      />
+      <Button variant='contained' sx={{
+        height : '55px',
+        border : 'none',
+        borderRadius : '0px 10px 10px 0px'
+      }}>Contact Us!</Button>
     </Box>
   )
 }
